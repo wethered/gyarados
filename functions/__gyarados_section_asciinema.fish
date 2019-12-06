9ff9593c4dd6f73b6df77e2e854666b06a203810
yamakadi <me@yamakadi.com>
2019-12-06 18:19:22 +0900

    Fix fix fix fix

--- functions/__gyarados_section_asciinema.fish
+++ functions/__gyarados_section_asciinema.fish
@@ -8,8 +8,8 @@ function __gyarados_section_asciinema -d "Display whether asciinema is currently
 	# ------------------------------------------------------------------------------
 
 	__gyarados_util_set_default GYARADOS_ASCIINEMA_SHOW true
-	__gyarados_util_set_default GYARADOS_ASCIINEMA_COLOR "red"
-	__gyarados_util_set_default GYARADOS_ASCIINEMA_SYMBOL "⊚ "
+	__gyarados_util_set_default GYARADOS_ASCIINEMA_COLOR red
+	__gyarados_util_set_default GYARADOS_ASCIINEMA_SYMBOL ⊚
 	__gyarados_util_set_default GYARADOS_ASCIINEMA_PREFIX $GYARADOS_PROMPT_DEFAULT_PREFIX
 	__gyarados_util_set_default GYARADOS_ASCIINEMA_SUFFIX $GYARADOS_PROMPT_DEFAULT_SUFFIX
 
@@ -18,12 +18,11 @@ function __gyarados_section_asciinema -d "Display whether asciinema is currently
 	# ------------------------------------------------------------------------------
 
 	[ $GYARADOS_ASCIINEMA_SHOW = false ]; and return
+	[ $ASCIINEMA_REC = false ]; and return
 
-	if test "$ASCIINEMA_REC" = "true"
-		__gyarados_lib_section \
-			$GYARADOS_ASCIINEMA_COLOR \
-			$GYARADOS_PROMPT_DEFAULT_PREFIX \
-			$GYARADOS_ASCIINEMA_SYMBOL \
-			$GYARADOS_ASCIINEMA_SUFFIX
-	end
+	__gyarados_lib_section \
+		$GYARADOS_ASCIINEMA_COLOR \
+		$GYARADOS_ASCIINEMA_PREFIX \
+		$GYARADOS_ASCIINEMA_SYMBOL \
+		$GYARADOS_ASCIINEMA_SUFFIX
 end
