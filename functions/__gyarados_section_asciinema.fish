bffe0eec853d2535a7c3f3b635c7bef009fcd40f
yamakadi <me@yamakadi.com>
2019-12-06 18:24:15 +0900

    Fix fix fix fix

--- functions/__gyarados_section_asciinema.fish
+++ functions/__gyarados_section_asciinema.fish
@@ -2,27 +2,32 @@
 # asciinema
 #
 
-function __gyarados_section_asciinema -d "Display whether asciinema is currently recording!"
+
+# If there is an ssh connections, current machine name.
+function __gyarados_section_host -d "Display whether asciinema is currently recording"
+
 	# ------------------------------------------------------------------------------
 	# Configuration
 	# ------------------------------------------------------------------------------
 
 	__gyarados_util_set_default GYARADOS_ASCIINEMA_SHOW true
-	__gyarados_util_set_default GYARADOS_ASCIINEMA_COLOR red
-	__gyarados_util_set_default GYARADOS_ASCIINEMA_SYMBOL ⊚
 	__gyarados_util_set_default GYARADOS_ASCIINEMA_PREFIX $GYARADOS_PROMPT_DEFAULT_PREFIX
 	__gyarados_util_set_default GYARADOS_ASCIINEMA_SUFFIX $GYARADOS_PROMPT_DEFAULT_SUFFIX
+	__gyarados_util_set_default GYARADOS_ASCIINEMA_SYMBOL "⊚"
+	__gyarados_util_set_default GYARADOS_ASCIINEMA_COLOR blue
+	__gyarados_util_set_default GYARADOS_ASCIINEMA_COLOR_SSH green
 
 	# ------------------------------------------------------------------------------
 	# Section
 	# ------------------------------------------------------------------------------
 
-	[ $GYARADOS_ASCIINEMA_SHOW = false ]; and return
-	[ $ASCIINEMA_REC = false ]; and return
+	[ "$GYARADOS_ASCIINEMA_SHOW" = false ]; and return
 
-	__gyarados_lib_section \
-		$GYARADOS_ASCIINEMA_COLOR \
-		$GYARADOS_ASCIINEMA_PREFIX \
-		$GYARADOS_ASCIINEMA_SYMBOL \
-		$GYARADOS_ASCIINEMA_SUFFIX
+	if set -q ASCIINEMA_REC;
+		__gyarados_lib_section \
+			$GYARADOS_ASCIINEMA_COLOR \
+			$GYARADOS_HOST_PREFIX \
+			"$GYARADOS_ASCIINEMA_SYMBOL" \
+			$GYARADOS_HOST_SUFFIX
+		end
 end
