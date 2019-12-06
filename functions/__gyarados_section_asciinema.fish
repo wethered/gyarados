a92651d19c11fb7e79c8427712700a1f8672e729
yamakadi <me@yamakadi.com>
2019-12-06 18:50:11 +0900

    Fix fix fix fix

--- functions/__gyarados_section_asciinema.fish
+++ functions/__gyarados_section_asciinema.fish
@@ -11,8 +11,8 @@ function __gyarados_section_asciinema -d "Display whether asciinema is currently
 	__gyarados_util_set_default GYARADOS_ASCIINEMA_SHOW true
 	__gyarados_util_set_default GYARADOS_ASCIINEMA_PREFIX $GYARADOS_PROMPT_DEFAULT_PREFIX
 	__gyarados_util_set_default GYARADOS_ASCIINEMA_SUFFIX $GYARADOS_PROMPT_DEFAULT_SUFFIX
-	__gyarados_util_set_default GYARADOS_ASCIINEMA_SYMBOL "⊚"
-	__gyarados_util_set_default GYARADOS_ASCIINEMA_COLOR red
+	__gyarados_util_set_default GYARADOS_ASCIINEMA_SYMBOL "⊚ "
+	__gyarados_util_set_default GYARADOS_ASCIINEMA_COLOR magenta
 
 	# ------------------------------------------------------------------------------
 	# Section
