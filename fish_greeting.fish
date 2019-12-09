2e784c182d6182847abaf03408bf10727677cbfe
yamakadi <me@yamakadi.com>
2019-12-09 13:19:38 +0900

    Configure auto-recording - debug

--- fish_greeting.fish
+++ fish_greeting.fish
@@ -2,18 +2,17 @@ function fish_greeting
   set_color $fish_color_autosuggestion
   
   if test -e ~/.config/kalima/record_session
-    set -l should_record (cat ~/.config/kalima/record_session)
+    set -gx GYARADOS_SHOULD_RECORD (cat ~/.config/kalima/record_session)
   else
-    set -l should_record "false"
+    set -gx GYARADOS_SHOULD_RECORD "false"
   end
 
-  if status is-interactive; and not set -q ASCIINEMA_REC; and test "$should_record" = "true"
+  if status is-interactive; and not set -q ASCIINEMA_REC; and test "$GYARADOS_SHOULD_RECORD" = "true"
     set -gx ASCIINEMA_REC 1
     asciinema rec (cat ~/.config/kalima/project_home)/1_evidence/screenshot_(date +%F_%H-%M-%S).cast
   end
 
-  echo "I am properly running! Here is the recording status:"
-  echo $should_record
+  echo "I am properly running! Here is the recording status: $GYARADOS_SHOULD_RECORD"
 
   set_color normal
 end
