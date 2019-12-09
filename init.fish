8cba15afab6df8674c17deea55cf700969d63b57
yamakadi <me@yamakadi.com>
2019-12-09 13:13:22 +0900

    Configure auto-recording

--- init.fish
+++ init.fish
@@ -1,1 +1,11 @@
-set HELLOWORLD "helloworld"
+
+if test -e ~/.config/kalima/record_session
+	set -l should_record (cat ~/.config/kalima/record_session)
+else
+	set -l should_record "false"
+end
+
+if status is-interactive; and not set -q ASCIINEMA_REC; and test "$should_record" = "true"
+    set -gx ASCIINEMA_REC 1
+    asciinema rec (cat ~/.config/kalima/project_home)/1_evidence/screenshot_(date +%F_%H-%M-%S).cast
+end
