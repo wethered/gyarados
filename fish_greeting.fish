8b202a8820d5e9ccd4d022012fc3477857f67f97
yamakadi <me@yamakadi.com>
2019-12-09 13:22:43 +0900

    Fix fix fix

--- fish_greeting.fish
+++ fish_greeting.fish
@@ -1,18 +1,7 @@
 function fish_greeting
   set_color $fish_color_autosuggestion
   
-  if test -e ~/.config/kalima/record_session
-    set -gx GYARADOS_SHOULD_RECORD (cat ~/.config/kalima/record_session)
-  else
-    set -gx GYARADOS_SHOULD_RECORD "false"
-  end
-
-  if status is-interactive; and not set -q ASCIINEMA_REC; and test "$GYARADOS_SHOULD_RECORD" = "true"
-    set -gx ASCIINEMA_REC 1
-    asciinema rec (cat ~/.config/kalima/project_home)/1_evidence/screenshot_(date +%F_%H-%M-%S).cast
-  end
-
-  echo "I am properly running! Here is the recording status: $GYARADOS_SHOULD_RECORD"
+  echo "Okay, let's go shopping!"
 
   set_color normal
 end
