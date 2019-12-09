96e660e1378d548a5470edc3ad13033d7484cd2f
yamakadi <me@yamakadi.com>
2019-12-09 13:18:11 +0900

    Configure auto-recording - debug

--- fish_greeting.fish
+++ fish_greeting.fish
@@ -1,5 +1,19 @@
 function fish_greeting
   set_color $fish_color_autosuggestion
-  echo "Okay, let's go shopping!"
+  
+  if test -e ~/.config/kalima/record_session
+    set -l should_record (cat ~/.config/kalima/record_session)
+  else
+    set -l should_record "false"
+  end
+
+  if status is-interactive; and not set -q ASCIINEMA_REC; and test "$should_record" = "true"
+    set -gx ASCIINEMA_REC 1
+    asciinema rec (cat ~/.config/kalima/project_home)/1_evidence/screenshot_(date +%F_%H-%M-%S).cast
+  end
+
+  echo "I am properly running! Here is the recording status:"
+  echo $should_record
+
   set_color normal
 end
--- init.fish
+++ init.fish
@@ -1,14 +1,2 @@
 
-if test -e ~/.config/kalima/record_session
-	set -l should_record (cat ~/.config/kalima/record_session)
-else
-	set -l should_record "false"
-end
-
-if status is-interactive; and not set -q ASCIINEMA_REC; and test "$should_record" = "true"
-    set -gx ASCIINEMA_REC 1
-    asciinema rec (cat ~/.config/kalima/project_home)/1_evidence/screenshot_(date +%F_%H-%M-%S).cast
-end
-
-echo "I am properly running! Here is the recording status:"
-echo $should_record
+echo "I don't seem to work for whatever reason."
